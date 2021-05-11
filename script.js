8594e5b2c1c8e7b1fbc577e217d294a7efbbb607
Oscar A.G <>
2021-05-11 19:24:32 +0000

    Spacing and basically the entire program so that the linter would be happy

--- script.js
+++ script.js
@@ -5,23 +5,19 @@ let rannum = 0
 let counter = 1
 
 function Begin () {
-  inputa = parseInt(inputa)
-  rannum = parseInt(rannum)
-  counter = parseInt(counter)
-  
   rannum = Math.floor(Math.random() * 10) + 1
+  counter = 1
 
-  while (inputa != rannum) { 
+   while (counter === 1) {
    inputa = prompt('Please guess a number between 1 and 10')
 
-    if (inputa > rannum) {
-     alert('incorrect guess, please try again, the number is bigger than what you guessed')
-   } else if (inputa < rannum) {  
-     alert('incorrect guess, please try again, the number is smaller than what you guessed')
+    if (inputa < rannum) {
+      alert('incorrect guess, please try again, the number is bigger than what you guessed')
+    } else if (inputa > rannum) {
+      alert('incorrect guess, please try again, the number is smaller than what you guessed')
+    } else {
+      alert('correct guess, congradulations')
+     counter = 2
+    }
    }
-  }
-
-  if (inputa == rannum) { 
-    alert('correct guess, congradulations')
-  }
 }
